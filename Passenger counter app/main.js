dc91b9aa228192b6df43a004e107847a94910045
Atharva Hinge <89994686+36atharva@users.noreply.github.com>
2022-11-05 11:02:16 +0530

    Update main.js

--- Passenger counter app/main.js
+++ Passenger counter app/main.js
@@ -1,7 +1,17 @@
-let countEl = document.getElementById("count-el")
-console.log(countEl)
 let count = 0
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+
 function increment() {
-    count = count + 1
-    countEl.innerText = count
+    count += 1
+    countEl.textContent = count
 }
+
+function save() {
+    let countStr = count + " - "
+    saveEl.textContent += countStr;
+    countEl.textContent = 0
+    count = 0
+}
+
+console.log("Let's count people on the subway!")
