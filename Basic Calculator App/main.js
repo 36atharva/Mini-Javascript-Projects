9bddc7e8229bed6227e31718b2de26ef7393fb1e
Atharva Hinge <89994686+36atharva@users.noreply.github.com>
2022-11-06 10:34:54 +0530

    Update main.js

---
+++ Basic Calculator App/main.js
@@ -0,0 +1,27 @@
+let num1 = 8
+let num2 = 2
+document.getElementById("num1-el").textContent = num1
+document.getElementById("num2-el").textContent = num2
+
+let sumEl = document.getElementById("sum-el")
+
+function add() {
+    let result = num1 + num2
+    sumEl.textContent = "Sum: " + result
+}
+
+function subtract() {
+    let result = num1 - num2
+    sumEl.textContent = "Sum: " + result
+}
+
+function divide() {
+    let result = num1 / num2
+    sumEl.textContent = "Sum: " + result
+}
+
+function multiply() {
+    let result = num1 * num2
+    sumEl.textContent = "Sum: " + result
+}
+
