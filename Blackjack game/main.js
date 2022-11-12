44a909255b45dd6bcc72e3533ecf1a9c23793d4b
Atharva Hinge <atharva.3936@gmail.com>
2022-11-12 14:56:24 +0530

    updated the code

--- Blackjack game/main.js
+++ Blackjack game/main.js
@@ -2,7 +2,7 @@ function randomIntFromInterval(min, max) { // min and max included
     return Math.floor(Math.random() * (max - min + 1) + min)
   }
 
-let firstCard = 6;
+let firstCard = 12;
 let secondCard = 9;
 let hasBlackJack = false;
 let isAlive = true;
@@ -11,16 +11,26 @@ let sum = firstCard + secondCard;
 
 let message = "";
 
-if (sum < 21) {
-    message = "Do you want to draw a new card? 🙂";
-}
-else if(sum === 21){
-    message = "Wohoo! You've got Blackjack! 🥳"
-    hasBlackJack = true;
-}
-else {
-    message = "You're out of the game! 😭";
-    isAlive = false;
-}
+let messageEl = document.getElementById("message-el");
+let sumEl = document.getElementById("sum-el");
+let cardEl = document.querySelector("#card-el");
 
-console.log(message);
+
+function startGame(){
+    if (sum < 21) {
+        message = "Do you want to draw a new card? 🙂";
+        
+    }
+    else if(sum === 21){
+        message = "Wohoo! You've got Blackjack! 🥳"
+        hasBlackJack = true;
+    }
+    else {
+        message = "You're out of the game! 😭";
+        isAlive = false;
+    }
+    console.log(message);
+    messageEl.textContent = message;
+    sumEl.textContent += sum;
+    cardEl.textContent = "Cards: " + firstCard + " and " + secondCard;
+}
