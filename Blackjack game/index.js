fbba7cb35e270dcce6b79f70b30f32ca24056933
Atharva Hinge <atharva.3936@gmail.com>
2022-11-12 13:55:45 +0530

    Updated the javascript files

---
+++ Blackjack game/index.js
@@ -0,0 +1,8 @@
+function randomIntFromInterval(min, max) { // min and max included 
+    return Math.floor(Math.random() * (max - min + 1) + min)
+  }
+
+let firstCard = 6;
+let secondCard = 9;
+
+let sum = firstCard + secondCard;
