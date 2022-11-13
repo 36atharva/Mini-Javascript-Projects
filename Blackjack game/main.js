486b56ac2fbca0be40c7c2044beefe1b70eff3de
Atharva Hinge <atharva.3936@gmail.com>
2022-11-13 12:56:38 +0530

    updated the project

--- Blackjack game/main.js
+++ Blackjack game/main.js
@@ -1,9 +1,10 @@
 function randomIntFromInterval(min, max) { // min and max included 
     return Math.floor(Math.random() * (max - min + 1) + min)
-  }
+}
 
 let firstCard = 12;
-let secondCard = 9;
+let secondCard = 5;
+let arrayCards = [firstCard, secondCard];
 let hasBlackJack = false;
 let isAlive = true;
 
@@ -15,9 +16,13 @@ let messageEl = document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el");
 let cardEl = document.querySelector("#card-el");
 
-
 function startGame(){
-    cardEl.textContent = "Cards: " + firstCard + " " + secondCard; 
+    renderGame();
+}
+
+
+function renderGame(){
+    cardEl.textContent = "Cards: " + arrayCards[0] + " " + arrayCards[1]; 
     sumEl.textContent = "Sum: " + sum;
     if (sum < 21) {
         message = "Do you want to draw a new card? 🙂";
@@ -31,7 +36,16 @@ function startGame(){
         message = "You're out of the game! 😭";
         isAlive = false;
     }
-    console.log(message);
+    // console.log(message);
     messageEl.textContent = message;
     
+}
+
+function newCard(){
+    console.log("New card drawn");
+    let cardNew = 4;
+    arrayCards.push(cardNew);
+    sum += cardNew;
+    renderGame();
+
 }
